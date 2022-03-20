58caa423861ac6602315c033c70d7ae473604953
Deepak Sharma <66753368+Ronin2494@users.noreply.github.com>
2022-03-19 21:45:25 -0400

    Add files via upload

---
+++ HTML Project 1/script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.cards')
+
+panels.forEach((cards) =>{
+    cards.addEventListener('click', () => {
+        removeActiveClasses()
+        cards.classList.add('active')        
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach(cards => {
+        cards.classList.remove('active')
+    })
+}
+
+
